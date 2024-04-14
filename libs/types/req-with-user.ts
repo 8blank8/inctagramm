d7316425f73066c5e9ba3b5b2d6faecee6a0a021
8blank8 <springfield.3298@gmail.com>
2024-04-14 15:35:32 +0700

    add: ReqWithGoogleUser type

--- libs/types/req-with-user.ts
+++ libs/types/req-with-user.ts
@@ -1,3 +1,6 @@
+import { GoogleUserOauthType } from "@libs/guards/google.guard"
 import { Request } from "express"
 
 export type ReqWithUser = Request & { userId: string, deviceId: string }
+
+export type ReqWithGoogleUser = Request & { user: GoogleUserOauthType }
