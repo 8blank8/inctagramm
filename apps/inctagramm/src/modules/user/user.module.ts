035c80ce1083a7e4801f3fca72551f68f6f4b3dc
8blank8 <springfield.3298@gmail.com>
2024-04-14 15:32:49 +0700

    add: dependencies for user.module

--- apps/inctagramm/src/modules/user/user.module.ts
+++ apps/inctagramm/src/modules/user/user.module.ts
@@ -3,17 +3,24 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserEntity } from "./entities/user.entity";
 import { CreateUserUseCase } from "./use-cases/create/create-user.use-case";
 import { UserRepository } from "./repository/user.repository";
+import { CreateUserGoogleOauthUseCase } from "./use-cases/create/create-user-google-ouath.use-case";
+import { CreateDeviceUseCase } from "../device/use-cases/create/create-device.use-case";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UserEntity])
+        TypeOrmModule.forFeature([UserEntity]),
     ],
     providers: [
         CreateUserUseCase,
-        UserRepository
+        CreateUserGoogleOauthUseCase,
+        CreateDeviceUseCase,
+        UserRepository,
+        JwtService
     ],
     exports: [
         CreateUserUseCase,
+        CreateUserGoogleOauthUseCase,
         UserRepository
     ]
 })
